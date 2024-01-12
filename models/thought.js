fd88cd015bb804e0955567b6c959d11282307be4
Matt Thompson <matthewthompson118@gmail.com>
2024-01-11 21:01:57 -0400

    modified Thought.js

--- models/thought.js
+++ models/thought.js
@@ -1,29 +1,27 @@
-const mongoose = require('mongoose');
-const ReactionSchema = require('./reaction');
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
 
-const thoughtSchema = new mongoose.Schema({
+const thoughtSchema = new Schema({
     thoughtText: {
         type: String,
         required: true,
         minlength: 1,
-        maxlength: 280,
+        maxlength: 280
     },
     createdAt: {
         type: Date,
         default: Date.now,
-        get: (timestamp) => new Date(timestamp).toLocaleString(),
+        // Use a getter method to format the timestamp on query
+        get: createdAtVal => dateFormat(createdAtVal)
     },
     username: {
         type: String,
-        required: true,
+        required: true
     },
-    reactions: [ReactionSchema],
+    // Use ReactionsSchema to validate data for a reply
+    reactions: [reactionSchema]
 });
 
-thoughtSchema.virtual('reactionCount').get(function () {
-    return this.reactions.length;
-});
-
-const Thought = mongoose.model('Though', thoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
