d8f792b4041ddab19474c7a32dc9c59754052e7b
Matt Thompson <matthewthompson118@gmail.com>
2024-01-15 17:24:14 -0400

    fixed mongodb connection issue

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/tech-blog', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
