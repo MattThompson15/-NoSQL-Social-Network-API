32ccf70ecd1693c408569c79d8fb315954429fe4
Matt Thompson <matthewthompson118@gmail.com>
2024-01-15 14:57:23 -0400

    fixed syntax issue in userRoutes

--- routes/api-routes/userRoutes.js
+++ routes/api-routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/userController');
+const { getAllUsers, getUserById, createUser, updateUserById, deleteUserById, addFriend, removeFriend } = require('../controllers/userController');
 
 router.route('/').get(getAllUsers).post(createUser);
 
-router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+router.route('/:userId').get(getUserById).put(updateUserById).delete(deleteUserById);
 
-router.route('/:userId/friends/:friendId').post(addFriend).delete(remoiveFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
