ee2c0c2ca370afd4c8b85bb9eb72fe3b1553de0f
Matt Thompson <matthewthompson118@gmail.com>
2024-01-15 13:23:42 -0400

    modified thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,6 +1,54 @@
 const { Thought, User } = require('../models');
 
-const thoughtController = {
+const ThoughtController = {
+    // Get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .then(thoughts => res.json(thoughts))
+        .catch(err => res.status(500).json(err));
+    },
+
+    // Get one thought by id
+    getThoughtById(req, res) {
+        Thought.findById(req.params.thoughtId)
+        .then(thought => thought ? res.json(thought) : res.status(404).json({ message: 'No thought found with this id!' }))
+        .catch(err => res.status(500).json(err));
+    },
+
+    // Create a thought
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then(thought => res.status(201).json(thought))
+        .catch(err => res.status(500).json(err));
+    },
+
+    // Delete a thought
+    deleteThought(req, res) {
+        Thought.findByIdAndDelete(req.params.thoughtId)
+        .then(thought => thought ? res.json({ message: 'Thought deleted!' }) : res.status(404).json({ message: 'No thought found with this id!' }))
+        .catch(err => res.status(500).json(err));
+    },
+
+    // Update a thought
+    updateThoughtById(req, res) {
+        Thought.findByIdAndUpdate(req.params.thoughtId, req.body, { new: true })
+        .then(thought => thought ? res.json(thought) : res.status(404).json({ message: 'No thought found with this id!' }))
+        .catch(err => res.status(500).json(err));
+    },
+
+    // Create a reaction
+    createReaction(req, res) {
+        Thought.findByIdAndUpdate(req.params.thoughtId, { $addToSet: { reactions: req.body } }, { new: true })
+        .then(thought => thought ? res.json(thought) : res.status(404).json({ message: 'No thought found with this id!' }))
+        .catch(err => res.status(500).json(err));
+    },
+
+    // Delete a reaction
+    deleteReaction(req, res) {
+        Thought.findByIdAndUpdate(req.params.thoughtId, { $pull: { reactions: { reactionId: req.params.reactionId } } }, { new: true })
+        .then(thought => thought ? res.json(thought) : res.status(404).json({ message: 'No thought found with this id!' }))
+        .catch(err => res.status(500).json(err));
+    },
 };
 
-module.exports = thoughtController;
+module.exports = ThoughtController;
